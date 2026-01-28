cbd1831bebff67948f4a7e8c284eca49ae44c6e3
Nabeel <ahmadnabeel634@gmail.com>
2026-01-28 16:23:50 +0500

    changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.moneymint.app',
   appName: 'MoneyMint',
-  webDir: 'public',
+  webDir: 'capacitor_assets',
   server: {
     url: 'https://moneymint.vercel.app/',
     androidScheme: 'https'
